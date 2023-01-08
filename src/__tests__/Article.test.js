8cecaca7dbd60d1e37e340c7438c228d42ea1a43
Walker Le <walker.le88@gmail.com>
2023-01-07 22:39:45 -0700

    commit tree 7402543e2c61b2b6531864902dc064803dca1580

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,11 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+  
+  const element = screen.queryByText("please pass this test");
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  // Debugger
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
